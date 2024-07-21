5828f7601f4aefd9a5728b593cda2c31c1c15bce
Mahip Jain <mahipjain2008@gmail.com>
2024-07-21 15:06:44 +0530

    User can play vs computer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import rockImg from "./assets/rock.png";
+import paperImg from "./assets/paper.png";
+import scissorsImg from "./assets/scissors.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [playerSelected, setplayerSelected] = useState(-1);
+	const [computerSelected, setComputerSelected] = useState(-1);
+	function imgClick(imgNum) {
+		setplayerSelected(imgNum);
+		setComputerSelected(Math.ceil(Math.random() * 3));
+	}
+	return (
+		<div className="App">
+			<div className="game">
+				<div>{}</div>
+				<div className="player">
+					{(playerSelected == 1 || playerSelected == -1) && (
+						<img
+							src={rockImg}
+							alt=""
+							onClick={() => {
+								imgClick(1);
+							}}
+						/>
+					)}
+					{(playerSelected == 2 || playerSelected == -1) && (
+						<img
+							src={paperImg}
+							alt=""
+							onClick={() => {
+								imgClick(2);
+							}}
+						/>
+					)}
+					{(playerSelected == 3 || playerSelected == -1) && (
+						<img
+							src={scissorsImg}
+							alt=""
+							onClick={() => {
+								imgClick(3);
+							}}
+						/>
+					)}
+				</div>
+				<div className="computer">
+					{computerSelected == -1 && <h1>waiting for player...</h1>}
+					{computerSelected == 1 && <img src={rockImg} alt="" />}
+					{computerSelected == 2 && <img src={paperImg} alt="" />}
+					{computerSelected == 3 && <img src={scissorsImg} alt="" />}
+				</div>
+				<button
+					className="button"
+					onClick={() => {
+						setComputerSelected(-1);
+						setplayerSelected(-1);
+					}}
+				>
+					Play Again
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
