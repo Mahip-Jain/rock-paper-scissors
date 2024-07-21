4e5f21541cf497bcc5d792aeef710f6907d07ed1
Mahip Jain <mahipjain2008@gmail.com>
2024-07-21 15:26:55 +0530

    Added score and win detection

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,37 @@ import scissorsImg from "./assets/scissors.png";
 function App() {
 	const [playerSelected, setplayerSelected] = useState(-1);
 	const [computerSelected, setComputerSelected] = useState(-1);
+	const [playerScore, setPlayerScore] = useState(0);
+	const [computerScore, setComputerScore] = useState(0);
+	const [winner, setWinner] = useState(-1);
 	function imgClick(imgNum) {
 		setplayerSelected(imgNum);
-		setComputerSelected(Math.ceil(Math.random() * 3));
+		var computer = Math.ceil(Math.random() * 3);
+		setComputerSelected(computer);
+		if (
+			(computer == 1 && imgNum == 3) ||
+			(computer == 2 && imgNum == 1) ||
+			(computer == 3 && imgNum == 2)
+		) {
+			setComputerScore(computerScore + 1);
+			setWinner(2);
+		} else if (computer == imgNum) {
+			setWinner(0);
+		} else {
+			setPlayerScore(playerScore + 1);
+			setWinner(1);
+		}
 	}
 	return (
 		<div className="App">
+			<div className="score">
+				<h2>Player Score: {playerScore}</h2>
+				<h2>Computer Score: {computerScore}</h2>
+			</div>
+			{winner == 0 && <h1>{"It's a tie"}</h1>}
+			{winner == 1 && <h1>You win!</h1>}
+			{winner == 2 && <h1>You lose!</h1>}
 			<div className="game">
-				<div>{}</div>
 				<div className="player">
 					{(playerSelected == 1 || playerSelected == -1) && (
 						<img
@@ -55,6 +78,7 @@ function App() {
 					onClick={() => {
 						setComputerSelected(-1);
 						setplayerSelected(-1);
+						setWinner(-1);
 					}}
 				>
 					Play Again
